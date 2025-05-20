d26dc7af6b3cc13619278bc012121344a34c4a99
Shivam Mishra <scm.mymail@gmail.com>
2025-05-20 14:42:11 +0530

    feat: more config

--- cms-kit.config.ts
+++ cms-kit.config.ts
@@ -5,8 +5,8 @@ export interface GlobalMeta {
   description?: string
   placeholder?: string
   fieldtype?: 'markdown' | 'textarea' | 'image'
+  unqiue?: boolean
   relation?: string
-  multiple?: boolean
 }
 
 export const seoFields = {
@@ -50,4 +50,78 @@ export const seoFields = {
     title: 'No Follow',
     description: 'Prevent search engines from following links on this page',
   }),
+}
+
+export const authorCollection = {
+  title: "Authors",
+  description: "Authors on the website",
+  fields: {
+    fullName: z.string(),
+    avatar: z.url().meta({
+      fieldType: 'image'
+    }),
+    bio: z.string().meta({
+      fieldType: 'textarea'
+    }),
+    slug: z.string().meta({
+      unqiue: true
+    }),
+  },
+  layout: {
+    list: ['fullName']
+  }
+}
+
+
+export const blogCollection = {
+  title: "Blog",
+  description: "A collection for blog posts",
+  fields: {
+    title: z.string().min(5).max(100).meta({
+      description: 'The title of the blog post'
+    }),
+    slug: z.string().min(5).max(100).meta({
+      description: 'URL-friendly version of the title',
+      unique: true,
+    }),
+    content: z.string().min(100).meta({
+      description: 'The main content of the blog post',
+      fieldtype: 'markdown'
+    }),
+    excerpt: z.string().max(200).optional().meta({
+      description: 'A short summary of the blog post',
+      fieldtype: 'textarea'
+    }),
+    publishedAt: z.date().meta({
+      title: 'Published Date',
+      description: 'When the post was published'
+    }),
+    featuredImage: z.string().url().optional().meta({
+      description: 'Main image for the blog post',
+      fieldtype: 'image'
+    }),
+    authors: z.array(z.string().meta({
+      relation: 'author'
+    })),
+    ...seoFields,
+  },
+  layout: {
+    list: [
+      'title', 'publishedAt'
+    ],
+    sidebar: [
+      'slug',
+      'author',
+      'excerpt',
+      'publishedAt',
+      'featuredImage',
+      ...Object.keys(seoFields)
+    ]
+  }
+}
+
+export default {
+  collections: {
+    blog: blogCollection
+  }
 }
