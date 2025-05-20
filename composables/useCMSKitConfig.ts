9af12ca24aefde6ba59866a3d0a536a63d3797b1
Shivam Mishra <scm.mymail@gmail.com>
2025-05-20 21:14:21 +0530

    feat: add sidebar

--- composables/useCMSKitConfig.ts
+++ composables/useCMSKitConfig.ts
@@ -1,7 +1,28 @@
+import { computed } from 'vue'
 import config from '~/cms-kit.config'
 
 export const useCMSKitConfig = () => {
+  const { collections, pages } = config
+
+  const collectionsItems = computed(() => {
+    return Object.entries(collections).map(([key, collection]) => ({
+      label: collection.title,
+      description: collection.description,
+      to: `/collections/${key}`,
+    }))
+  })
+
+  const pageItems = computed(() => {
+    return Object.entries(pages).map(([key, page]) => ({
+      label: page.title,
+      description: page.description,
+      to: key,
+    }))
+  })
+
   return {
+    collectionsItems,
+    pageItems,
     appName: config.appName || 'CMS Kit',
     ...config,
   }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { NavigationMenuItem } from '@nuxt/ui'
 
-// const { appName, collections, pages } = useCMSKitConfig()
+const { appName, collectionsItems, pageItems } = useCMSKitConfig()
 
 useHead({
   bodyAttrs: {
@@ -12,45 +12,34 @@ useHead({
 const items = ref<NavigationMenuItem[][]>([
   [
     {
-      label: 'Links',
-      type: 'label',
+      label: 'Home',
+      icon: 'i-lucide-layout-dashboard',
+      to: '/',
     },
     {
+      defaultOpen: true,
       label: 'Collections',
       icon: 'i-lucide-database',
-      children: [
-        {
-          label: 'Introduction',
-          description: 'Fully styled and customizable components for Nuxt.',
-          icon: 'i-lucide-house',
-        },
-      ],
+      children: collectionsItems.value,
     },
     {
+      defaultOpen: true,
       label: 'Pages',
       icon: 'i-lucide-file-text',
-      children: [
-        {
-          label: 'defineShortcuts',
-          icon: 'i-lucide-file-text',
-          description: 'Define shortcuts for your application.',
-          to: '/composables/define-shortcuts',
-        },
-      ],
+      children: pageItems.value,
     },
   ],
   [
     {
       label: 'GitHub',
       icon: 'i-simple-icons-github',
-      badge: '3.8k',
-      to: 'https://github.com/nuxt/ui',
+      badge: 'MIT License',
+      to: 'https://github.com/scmmishra/cms-kit',
       target: '_blank',
     },
     {
-      label: 'Help',
-      icon: 'i-lucide-circle-help',
-      disabled: true,
+      label: 'Log out',
+      icon: 'i-lucide-log-out',
     },
   ],
 ])
@@ -59,10 +48,13 @@ const items = ref<NavigationMenuItem[][]>([
 <template>
   <div class="flex">
     <aside class="sticky top-0 h-screen basis-48 border-r border-gray-200">
+      <div class="flex-grow-0 px-2.5 py-2 font-bold">
+        {{ appName }}
+      </div>
       <UNavigationMenu
         orientation="vertical"
         :items="items"
-        class="data-[orientation=vertical]:w-48 h-full [&>*:first-child]:flex-grow [&>*:first-child]:h-full"
+        class="data-[orientation=vertical]:w-48 pb-3 h-[calc(100vh-40px)] [&>*:second-child]:flex-grow [&>*:second-child]:h-full"
       />
     </aside>
     <main class="basis-0 flex-grow-[999] [min-inline-size_50%] min-h-screen py-3">
