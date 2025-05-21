5818fdca9596b1a95a057630ddb1accc1727b3b4
Shivam Mishra <scm.mymail@gmail.com>
2025-05-21 12:22:35 +0530

    feat: kickstart collection

---
+++ pages/collection/[collectionName]/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useCollection } from '~/composables/useCollection'
+
+const { collectionName, collection } = useCollection()
+
+const collectionSchema = computed(() => collection.value?.fields)
+</script>
+
+<!-- eslint-disable vue/no-multiple-template-root -->
+<template>
+  <h1 class="font-bold text-black mb-2">
+    {{ collection.title || collectionName }}
+  </h1>
+  <section>
+    <div
+      v-for="(field, key) in collectionSchema"
+      :key="key"
+    >
+      <label>{{ field }}</label>
+    </div>
+  </section>
+</template>
