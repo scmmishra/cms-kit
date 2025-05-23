ba0850bbc7d9342b17e770908f4900b19e1745ad
Shivam Mishra <scm.mymail@gmail.com>
2025-05-23 17:58:10 +0530

    feat: markdown

--- components/MilkdownEditor.vue
+++ components/MilkdownEditor.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
 import { Milkdown, useEditor } from '@milkdown/vue'
-import { Editor, rootCtx } from '@milkdown/kit/core'
-import { listener, listenerCtx } from '@milkdown/plugin-listener'
+import { defaultValueCtx, Editor, rootCtx } from '@milkdown/kit/core'
+// import { listener, listenerCtx } from '@milkdown/plugin-listener'
+import { nord } from '@milkdown/theme-nord'
 import { commonmark } from '@milkdown/kit/preset/commonmark'
 
-const modelValue = defineModel<string>('modelValue')
+const modelValue = defineModel<string>('modelValue', {
+  default: '',
+})
 
 useEditor((root) => {
   return Editor.make()
+    .config(nord)
     .config((ctx) => {
       ctx.set(rootCtx, root)
-      const listener = ctx.get(listenerCtx)
+      ctx.set(defaultValueCtx, modelValue.value)
+      // const listener = ctx.get(listenerCtx)
 
-      listener.markdownUpdated((ctx, markdown, prevMarkdown) => {
-        if (markdown !== prevMarkdown) {
-          modelValue.value = markdown
-        }
-      })
+      // listener.markdownUpdated((ctx, markdown, prevMarkdown) => {
+      //   if (markdown !== prevMarkdown) {
+      //     modelValue.value = markdown
+      //   }
+      // })
     })
-    .use(listener)
+    // .use(listener)
     .use(commonmark)
 })
 </script>
