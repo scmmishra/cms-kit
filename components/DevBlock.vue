e64e9b7e37c6c645fa462bf9a491ec3c1c8d581d
Shivam Mishra <scm.mymail@gmail.com>
2025-05-23 17:17:21 +0530

    feat: allow hiding dev block

--- components/DevBlock.vue
+++ components/DevBlock.vue
@@ -1,10 +1,16 @@
 <script setup lang='ts'>
 const isDev = import.meta.dev
+
+const route = useRoute()
+
+const showBlock = computed(() => {
+  return isDev && route.query.mode !== 'preview'
+})
 </script>
 
 <template>
   <div
-    v-if="isDev"
+    v-if="showBlock"
     class="my-2 text-xs bg-white p-2 rounded-md outline outline-gray-200 overflow-hidden"
   >
     <pre><slot /></pre>
