eeaa4293ae5ca7a3348bf160c327fb6c950ffbc1
Shivam Mishra <scm.mymail@gmail.com>
2025-05-25 16:52:12 +0530

    feat: remove milkdown

--- components/form/fields/Markdown.vue
+++ components/form/fields/Markdown.vue
@@ -1,23 +1,7 @@
 <script setup lang="ts">
-import { MilkdownProvider } from '@milkdown/vue'
-import MilkdownEditor from '../../MilkdownEditor.vue'
-
 const modelValue = defineModel<string>('modelValue')
 </script>
 
 <template>
-  <MilkdownProvider class="prose dark:prose-invert prose-xs max-w-none w-full">
-    <MilkdownEditor v-model="modelValue" />
-  </MilkdownProvider>
+  <textarea v-model="modelValue" />
 </template>
-
-<style>
-@reference "@/assets/css/main.css";
-
-.ProseMirror.editor {
-  min-height: 20rem;
-  max-height: 30rem;
-  overflow: scroll;
-  @apply w-full max-w-none w-full rounded-md border-0 placeholder:text-dimmed focus:outline-none disabled:cursor-not-allowed disabled:opacity-75 transition-colors px-2.5 py-1.5 text-sm gap-1.5 text-highlighted bg-default ring ring-inset ring-accented focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-primary;
-}
-</style>
