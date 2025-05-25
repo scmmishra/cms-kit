4506bb14286139b9a7e14f8244b3eb1c1f89673c
Shivam Mishra <scm.mymail@gmail.com>
2025-05-25 18:36:24 +0530

    feat: add nuxt auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui'],
+  modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', 'nuxt-auth-utils'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   ui: {
